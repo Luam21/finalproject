e3c9c1182d36c13fe23016f560034836c53a3409
Luam Redai <luammeharii21@gmail.com>
2023-12-08 23:26:35 -0500

    finalized

--- firstbot.py
+++ firstbot.py
@@ -1,34 +1,46 @@
-import discord 
-import os 
-import random 
-from dotenv import load_dotenv 
-load_dotenv() 
+import os
+import discord
+from ec2_metadata  import ec2_metadata
+from dotenv import load_dotenv
 
-client = discord.Client() 
-token = os.getenv('TOKEN')
-@client.event 
-async def on_ready(): 
-	print("Logged in as a bot {0.user}".format(client))
-@client.event 
-async def on_message(message): 
-	username = str(message.author).split("#")[0] 
-	channel = str(message.channel.name) 
-	user_message = str(message.content) 
+load_dotenv()
+client = discord.Client()
 
-	print(f'Message {user_message} by {username} on {channel}') 
+token = str(os.getenv('TOKEN'))
 
-	if message.author == client.user: 
-		return
 
-	if channel == "random": 
-		if user_message.lower() == "hello" or user_message.lower() == "hi": 
-			await message.channel.send(f'Hello {username}') 
-			return
-		elif user_message.lower() == "bye": 
-			await message.channel.send(f'Bye {username}') 
-		elif user_message.lower() == "tell me a joke": 
-			jokes = [" Can someone please shed more light on how my lamp got stolen?", 
-					"Why is she called llene? She stands on equal legs.", 
-					"What do you call a gazelle in a lions territory? Denzel."] 
-			await message.channel.send(random.choice(jokes)) 
+@client.event
+async def on_ready():
+    try:
+        print("Logged in as a bot {0.user}".format(client))
+    except Exception as error:
+        pass
+
+@client.event
+async def on_message(message):
+    username = str(message.author).split("#")[0]
+    channel = str(message.channel.name)
+    user_message = str(message.content)
+
+    print(f'{username}: {user_message} in channel: #{channel}')
+
+    if message.author == client.user:
+        return
+
+    if channel == "random":
+        if user_message.lower() == 'hello world':
+            await message.channel.send('hello')
+
+        elif user_message.lower() == ("tell me about your server"):
+
+            await message.channel.send(f"""your ec2 server data:\n
+region:{ec2_metadata.region}\n
+public ipv4 address:{ec2_metadata.public_ipv4}\n
+availability zone:{ec2_metadata.availability_zone}\n
+server instance:{ec2_metadata.instance_type}
+""")
+        else:
+            await message.channel.send(f"I'm sorry, the command '{user_message}' is not a valid command")
+
+
 client.run(token)
